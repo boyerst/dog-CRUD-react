05670e5838408632e0e876e15a05a31a9b4c9fce
Steve Boyer <steveboyer@Macbook-2.local>
2020-04-21 18:26:17 -0500

    created react app, created DogContainer component, rendered DogContainer in App

---
+++ react-dog-crud/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import DogContainer from './DogContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Dog CRUD</h1>
+      <div>
+        <DogContainer />
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ react-dog-crud/src/DogContainer/index.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import './index.css'
+
+class DogContainer extends React.Component {
+  render() {
+    return (
+    <h1>DogContainer</h1>
+    )
+  }
+}
+
+export default DogContainer
